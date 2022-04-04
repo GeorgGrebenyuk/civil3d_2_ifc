ef91760327520a80a2bdcf80bd4c972a0d4f265c
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-04-05 00:53:19 +0300

    Update BaseStructures.cs
    Change logic to class instead static methods

--- src/civil2ifc/ifc/BaseStructures.cs
+++ src/civil2ifc/ifc/BaseStructures.cs
@@ -12,6 +12,7 @@ using Autodesk.AutoCAD.DatabaseServices;
 using cas = Autodesk.Civil.ApplicationServices;
 using cds = Autodesk.Civil.DatabaseServices;
 using static civil2ifc.Start;
+using Autodesk.AutoCAD.Geometry;
 
 namespace civil2ifc.ifc
 {
@@ -21,7 +22,8 @@ namespace civil2ifc.ifc
         {
             return new IfcCartesianPoint(ifc_db, x, y, z);
         }
-        public static IfcFace ifc_face (cds.TinSurfaceTriangle c3d_triangle)
+        public IfcFace face;
+        public BaseStructures (cds.TinSurfaceTriangle c3d_triangle)
         {
             IfcCartesianPoint get_by_vertex(cds.TinSurfaceVertex vertex)
             {
@@ -35,8 +37,18 @@ namespace civil2ifc.ifc
 
             IfcPolyLoop ifc_loop = new IfcPolyLoop(ifc_3points);
             IfcFaceOuterBound ifc_bound = new IfcFaceOuterBound(ifc_loop, true);
-            //IfcFaceBound ifc_bound = new IfcFaceBound(ifc_loop, true);
-            return new IfcFace(ifc_bound);
+            this.face =  new IfcFace(ifc_bound);
+        }
+        public BaseStructures (List<Point3d> points)
+        {
+            IfcCartesianPoint[] ifc_3points = new IfcCartesianPoint[points.Count];
+            for (int i1 = 0; i1 < points.Count; i1++)
+            {
+                ifc_3points[i1] = new IfcCartesianPoint(ifc_db, points[i1].X, points[i1].Y, points[i1].Z);
+            }
+            IfcPolyLoop ifc_loop = new IfcPolyLoop(ifc_3points);
+            IfcFaceOuterBound ifc_bound = new IfcFaceOuterBound(ifc_loop, true);
+            this.face = new IfcFace(ifc_bound);
         }
 
     }
