3c1891c373649728ce73fdcfcc98af9ac06cfcd8
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-04-05 00:53:55 +0300

    Create SetMaterial.cs
    New class for getting info about object colo and (in future) material to Ifc's styles

---
+++ src/civil2ifc/civil_properties/SetMaterial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+
+using cas = Autodesk.Civil.ApplicationServices;
+using cds = Autodesk.Civil.DatabaseServices;
+using GeometryGym.Ifc;
+
+using static civil2ifc.Start;
+
+namespace civil2ifc.civil_properties
+{
+    public class SetMaterial
+    {
+        //private IfcObject base_object;
+        public IfcPresentationStyleAssignment style_assignm;
+        public SetMaterial(Autodesk.AutoCAD.Colors.Color color)
+        {
+            IfcColourRgb ifc_color = new IfcColourRgb(ifc_db, color.Red, color.Green, color.Blue);
+            IfcSurfaceStyleShading ifc_style = new IfcSurfaceStyleShading(ifc_color);
+            IfcSurfaceStyle ifc_style2 = new IfcSurfaceStyle(ifc_style);
+            this.style_assignm = new IfcPresentationStyleAssignment(ifc_style2);
+            
+        }
+    }
+}
