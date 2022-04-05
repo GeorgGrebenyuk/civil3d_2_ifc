2172b6430850fc405488bc57e4c9c64849e654db
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-04-05 17:24:21 +0300

    New version of algo with stl ...

--- src/civil2ifc/ifc/GetSolid.cs
+++ src/civil2ifc/ifc/GetSolid.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 using Autodesk.AutoCAD.ApplicationServices;
@@ -15,41 +16,50 @@ using GeometryGym.Ifc;
 
 using static civil2ifc.Start;
 using Autodesk.AutoCAD.Geometry;
+using stl = QuantumConcepts.Formats.StereoLithography;
 
 namespace civil2ifc.ifc
 {
     public class GetSolid
     {
         public IfcFaceBasedSurfaceModel surf_row;
-        public GetSolid(Solid3d input_solid)
+        public IfcPolygonalFaceSet surf_row2;
+        public GetSolid(ObjectId input_solid_id)
         {
-            //FullSubentityPath path = new FullSubentityPath(new ObjectId[1] { input_solid.Id }, new SubentityId(SubentityType.Null, IntPtr.Zero));
-            List<IfcFace> surf_faces = new List<IfcFace>();
-            
-
-            using (Brep brep = new Brep(input_solid))
+            using (DocumentLock acDocLock = ac_doc.LockDocument())
             {
-                BrepFaceCollection brep_faces = brep.Faces;
-                int v_count = brep_faces.Count();
-                foreach (Autodesk.AutoCAD.BoundaryRepresentation.Face face in brep_faces)
+                using (Transaction acTrans = ac_db.TransactionManager.StartTransaction())
                 {
-                    foreach (BoundaryLoop lp in face.Loops)
-                    {
-                        List<Point3d> face_points = new List<Point3d>();
-                        foreach (Edge edg in lp.Edges)
-                        {
-                            if (!face_points.Contains(edg.Vertex1.Point)) face_points.Add(edg.Vertex1.Point);
-                            if (!face_points.Contains(edg.Vertex2.Point)) face_points.Add(edg.Vertex2.Point);
-                        }
-                        if (face_points.Count() > 2) surf_faces.Add(new ifc.BaseStructures(face_points).face);
-                    }
-                   
+                    List<IfcFace> surf_faces = new List<IfcFace>();
 
-                    
-                    //face.Dispose();
+                    string solid_path = $@"C:\Users\{Environment.UserName}\AppData\Local\Temp\{Guid.NewGuid()}.stl";
+                    Solid3d input_solid = acTrans.GetObject(input_solid_id, OpenMode.ForRead) as Solid3d;
+                    input_solid.StlOut(solid_path, true);
+                    stl.STLDocument solid_stl = stl.STLDocument.Open(solid_path);
+                    List<stl.Facet> stl_facets = solid_stl.Facets.ToList();
+                    foreach (stl.Facet stl_one_facet in stl_facets)
+                    {
+                        surf_faces.Add(new ifc.BaseStructures(stl_one_facet).face);
+                    }
+                    File.Delete(solid_path);
+                    this.surf_row = new IfcFaceBasedSurfaceModel(new IfcConnectedFaceSet(surf_faces));
+                    acTrans.Commit();
                 }
             }
-            surf_row = new IfcFaceBasedSurfaceModel(new IfcConnectedFaceSet(surf_faces));
+            //FullSubentityPath path = new FullSubentityPath(new ObjectId[1] { input_solid.Id }, new SubentityId(SubentityType.Null, IntPtr.Zero));
+
+
+
+            //List<Tuple<double, double, double>> points_temp = new List<Tuple<double, double, double>>();
+            //List<IfcIndexedPolygonalFace> faces_indexed = new List<IfcIndexedPolygonalFace>();
+            //List<Point3d> face_points = new List<Point3d>();
+
+
+
+
+            //IfcCartesianPointList3D collection = new IfcCartesianPointList3D(ifc_db, points_temp);
+            //this.surf_row = new IfcPolygonalFaceSet(collection, faces_indexed);
+            
         }
     }
 }
