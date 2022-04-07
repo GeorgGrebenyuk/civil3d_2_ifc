60ec34c6828dfb19835f7f0094fadc115cbf760e
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-04-07 10:15:59 +0300

    Update GetSolid.cs
    Try-5 (brep)

--- src/civil2ifc/ifc/GetSolid.cs
+++ src/civil2ifc/ifc/GetSolid.cs
@@ -17,12 +17,13 @@ using GeometryGym.Ifc;
 using static civil2ifc.Start;
 using Autodesk.AutoCAD.Geometry;
 using stl = QuantumConcepts.Formats.StereoLithography;
+using dxf = netDxf;
 
 namespace civil2ifc.ifc
 {
     public class GetSolid
     {
-        public IfcFaceBasedSurfaceModel surf_row;
+        public IfcPolygonalFaceSet surf_row;
         //public IfcFaceBasedSurfaceModel surf_row;
         public GetSolid(ObjectId input_solid_id)
         {
@@ -31,64 +32,60 @@ namespace civil2ifc.ifc
             {
                 using (Transaction acTrans = ac_db.TransactionManager.StartTransaction())
                 {
-                    List<IfcFace> surf_faces = new List<IfcFace>();
-
-                    string folder_path = $@"C:\Users\{Environment.UserName}\AppData\Local\Temp\DXFs";
-                    if (!Directory.Exists(folder_path)) Directory.CreateDirectory(folder_path);
-                    string solid_path = $@"{folder_path}\{Guid.NewGuid()}.dxf";
                     Solid3d input_solid = acTrans.GetObject(input_solid_id, OpenMode.ForRead) as Solid3d;
 
-                    using (var db = new Database(false, true))
+                    List<Tuple<double, double, double>> points_temp = new List<Tuple<double, double, double>>();
+                    List<IfcIndexedPolygonalFace> faces_indexed = new List<IfcIndexedPolygonalFace>();
+                    List<Point3d> face_points = new List<Point3d>();
+
+                   
+                    using (Brep brp = new Brep(input_solid))
                     {
-                        // read the template dwg file
-                        string tmp_path = @"C:\Users\Georg\AppData\Local\Autodesk\C3D 2022\enu\Template\acadiso.dwt";
-                        db.ReadDwgFile(tmp_path, FileOpenMode.OpenForReadAndAllShare, false, null);
-
-                        // start a transaction
-                        using (var tr = db.TransactionManager.StartTransaction())
-                        {
-                            // check if a layer named "Character" already exists and create it if not
-                            string layerName = "solid";
-                            ObjectId layerId;
-                            var layerTable = (LayerTable)tr.GetObject(db.LayerTableId, OpenMode.ForRead);
-                            if (layerTable.Has(layerName))
+                        for (int complex_counter = 0; complex_counter < brp.Complexes.Count(); complex_counter++)
+                        {                            
+                            Complex cmp = brp.Complexes.ElementAt(complex_counter);
+                            for (int shell_counter = 0; shell_counter < cmp.Shells.Count(); shell_counter++)
                             {
-                                layerId = layerTable[layerName];
-                            }
-                            else
-                            {
-                                tr.GetObject(db.LayerTableId, OpenMode.ForWrite);
-                                var layer = new LayerTableRecord
+                                Shell shl = cmp.Shells.ElementAt(shell_counter);
+                                for (int face_counter = 0; face_counter < shl.Faces.Count(); face_counter++)
                                 {
-                                    Name = layerName,
-                                };
-                                layerId = layerTable.Add(layer);
-                                tr.AddNewlyCreatedDBObject(layer, true);
-                            }
-
-                            // get the Model space
-                            var modelSpace = (BlockTableRecord)tr.GetObject(
-                                SymbolUtilityServices.GetBlockModelSpaceId(db), OpenMode.ForWrite);
-
-
-                            modelSpace.AppendEntity(input_solid);
-                            tr.AddNewlyCreatedDBObject(input_solid, true);
-
-                            // save changes to the database
-                            tr.Commit();
+                                    Autodesk.AutoCAD.BoundaryRepresentation.Face fce = shl.Faces.ElementAt(face_counter);
+                                    List<int> coord_indexes = new List<int>(); // КУДА ЭТО????
+                                    for (int BoundaryLoop_counter = 0; BoundaryLoop_counter < fce.Loops.Count(); BoundaryLoop_counter++)
+                                    {
+                                        BoundaryLoop lp = fce.Loops.ElementAt(BoundaryLoop_counter);
+                                        for (int edge_counter = 0; edge_counter < lp.Edges.Count(); edge_counter++)
+                                        {
+                                            Edge edg = lp.Edges.ElementAt(edge_counter);
+                                            vertex_opeations(edg.Vertex1);
+                                            vertex_opeations(edg.Vertex2);
+                                            void vertex_opeations(Autodesk.AutoCAD.BoundaryRepresentation.Vertex v)
+                                            {
+                                                if (!face_points.Contains(v.Point))
+                                                {
+                                                    face_points.Add(v.Point);
+                                                    points_temp.Add(new Tuple<double, double, double>(v.Point.X, v.Point.Y, v.Point.Z));
+                                                    coord_indexes.Add(points_temp.Count() - 1);
+                                                }
+                                                else
+                                                {
+                                                    int point_last = face_points.FindIndex(a => a == v.Point);
+                                                    coord_indexes.Add(point_last);
+                                                }
+                                            }
+                                        }
+                                    }
+                                    faces_indexed.Add(new IfcIndexedPolygonalFace(ifc_db, coord_indexes));
+                                }
+                            }                            
                         }
-
-                        // save the new drawing
-                        db.DxfOut(solid_path, 0, DwgVersion.AC1027);
                     }
-
-                    
-                    //File.Delete(solid_path);
-                    this.surf_row = new IfcFaceBasedSurfaceModel(new IfcConnectedFaceSet(surf_faces));
+                    IfcCartesianPointList3D collection = new IfcCartesianPointList3D(ifc_db, points_temp);
+                    this.surf_row = new IfcPolygonalFaceSet(collection, faces_indexed);
                     acTrans.Commit();
                 }
             }
-            
+
         }
     }
 }
