70c65080866f9af519be898b61dbf793cdf4a14c
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-04-07 22:17:57 +0300

    Update BaseStructures.cs
    Add solid's method and remove STL's

--- src/civil2ifc/ifc/BaseStructures.cs
+++ src/civil2ifc/ifc/BaseStructures.cs
@@ -8,12 +8,12 @@ using GeometryGym.Ifc;
 
 using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.BoundaryRepresentation;
 
 using cas = Autodesk.Civil.ApplicationServices;
 using cds = Autodesk.Civil.DatabaseServices;
 using static civil2ifc.Start;
 using Autodesk.AutoCAD.Geometry;
-using stl = QuantumConcepts.Formats.StereoLithography;
 
 namespace civil2ifc.ifc
 {
@@ -53,22 +53,7 @@ namespace civil2ifc.ifc
             IfcFaceOuterBound ifc_bound = new IfcFaceOuterBound(ifc_loop, true);
             this.face = new IfcFace(ifc_bound);
         }
-        public BaseStructures(stl.Facet stl_one_facet)
-        {
-            IfcCartesianPoint[] ifc_3points = new IfcCartesianPoint[stl_one_facet.Vertices.Count()];
-            for (int i1 = 0; i1 < stl_one_facet.Vertices.Count; i1++)
-            {
-                ifc_3points[i1] = new IfcCartesianPoint(ifc_db, 
-                    stl_one_facet.Vertices[i1].X, stl_one_facet.Vertices[i1].Y, stl_one_facet.Vertices[i1].Z);
-            }
-            IfcPolyLoop ifc_loop = new IfcPolyLoop(ifc_3points);
-            IfcFaceOuterBound ifc_bound = new IfcFaceOuterBound(ifc_loop, true);
-            this.face = new IfcFace(ifc_bound);
-            foreach (stl.Vertex one_point in stl_one_facet.Vertices)
-            {
-
-            }
-        }
+        
         public BaseStructures (List<Point3d> points, List<List<int>> faces_indexed)
         {
             List<IfcCartesianPoint> ifc_points = new List<IfcCartesianPoint>();
@@ -111,6 +96,69 @@ namespace civil2ifc.ifc
             this.faces.Add(new IfcFace(new IfcFaceOuterBound(new IfcPolyLoop(new List<IfcCartesianPoint> { p1, p5, p6, p2 }), true)));
             this.faces.Add(new IfcFace(new IfcFaceOuterBound(new IfcPolyLoop(new List<IfcCartesianPoint> { p5, p6, p8, p7 }), true)));
         }
+        public BaseStructures (Solid3d input_solid)
+        {
+            //Temporal containers for external surface definition (faces indexes and Point3d list)
+            List<List<int>> faces_indexed = new List<List<int>>();
+            List<Point3d> points_temp = new List<Point3d>();
 
+            double length = input_solid.GeometricExtents.MinPoint.GetVectorTo(input_solid.GeometricExtents.MaxPoint).Length;
+
+            this.faces = new List<IfcFace>();
+            try
+            {
+                using (Brep brep = new Brep(input_solid))
+                {
+                    using (Mesh2dControl mc = new Mesh2dControl())
+                    {
+                        //Set mesh control about how to strip the mesh
+                        mc.MaxNodeSpacing = length;
+                        mc.MaxSubdivisions = 1;
+                        using (Mesh2dFilter mf = new Mesh2dFilter())
+                        {
+                            mf.Insert(brep, mc);
+                            using (Mesh2d m = new Mesh2d(mf))
+                            {
+                                Dictionary<Point3d, IfcCartesianPoint> pts = new Dictionary<Point3d, IfcCartesianPoint>();
+                                foreach (Element2d e in m.Element2ds)
+                                {
+                                    foreach (Node n in e.Nodes)
+                                    {
+                                        if (pts.ContainsKey(n.Point))
+                                            continue;
+
+                                        //Get each point
+                                        pts.Add(n.Point, new IfcCartesianPoint(ifc_db, n.Point.X, n.Point.Y, n.Point.Z));
+                                        n.Dispose();
+                                    }
+                                    e.Dispose();
+                                }
+
+                                foreach (Element2d e in m.Element2ds)
+                                {
+                                    List<IfcCartesianPoint> points_face = new List<IfcCartesianPoint>();
+                                    foreach (Node n in e.Nodes)
+                                    {
+                                        //Group each polygon
+                                        points_face.Add(pts[n.Point]);
+                                    }
+                                    IfcFaceOuterBound outerBound = new IfcFaceOuterBound(new IfcPolyLoop(points_face), true);
+                                    this.faces.Add(new IfcFace(outerBound));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                Extents3d solid_extent = input_solid.GeometricExtents;
+                this.faces = new ifc.BaseStructures(solid_extent).faces;
+            }
+        }
+        public BaseStructures (Point3d center, double height)
+        {
+            
+        }
     }
 }
